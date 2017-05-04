31448eec20c72a29adf9483f45f6c8bc178f73cb
Danu Widatama <widatama@gmail.com>
2017-05-04 09:40:13 +0700

    Set up production assets compression

--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -1,12 +1,26 @@
 const path = require('path');
 
+const Webpack = require('webpack');
+const OptimizeCSSPlugin = require('optimize-css-assets-webpack-plugin');
+
 const webpackConfig = require('./webpack.config');
 const appConfig = require('./app.config');
 
 webpackConfig.output = {
   path: path.resolve('', `./${appConfig.paths.dist.path}/`),
   publicPath: '',
-  filename: `${appConfig.dist.javascriptsPath}/${appConfig.bundleNames.js}`,
+  filename: `${appConfig.paths.dist.javascriptsPath}/${appConfig.bundleNames.js}`,
 };
+
+webpackConfig.plugins = (webpackConfig.plugins || []).concat([
+  new OptimizeCSSPlugin({
+    cssProcessorOptions: { discardComments: { removeAll: true } },
+  }),
+  new Webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+  }),
+]);
 
 module.exports = webpackConfig;
