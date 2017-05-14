31c6332429d5bc6cdfdba6234dc9b3f2b13a24b8
Danu Widatama <widatama@gmail.com>
2017-05-14 20:37:17 +0700

    Create number engine

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
 import './vueConfig';
 
+import Game from './modules/numberEngine';
+
 import AppComponent from './components/app.vue';
 
 global.App = new Vue({
   el: '#vue_app',
   render: h => h(AppComponent),
 });
+
+global.Game = Game;
--- test/unit/test.js
+++
@@ -1,9 +0,0 @@
-import tape from 'tape';
-
-import text from '../../src/javascripts/importMe';
-
-tape('Test', (assert) => {
-  assert.pass('Just pass');
-  assert.notEqual(text, '', `Imported ‘${text}’`);
-  assert.end();
-});
