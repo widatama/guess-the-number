ed10b4f7c5f524ed2ddc0a1e67c8802067cca010
Danu Widatama <widatama@gmail.com>
2021-04-25 13:12:40 +0700

    Update preview

--- service-worker.js
+++ service-worker.js
@@ -11,15 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b0b2504743cc50d70c7cbe6af661a37d.js"
+  "precache-manifest.de0e3856c75441b031818e9ec7949e2b.js"
 );
 
-workbox.core.skipWaiting();
+workbox.core.setCacheNameDetails({prefix: "guess-the-number"});
 
-workbox.core.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
